07cd58400f441288ab0b300fbd8e2040b3d4d0c2
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-09-08 23:50:18 +0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -4,3 +4,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Import and use other components here */}
+    </div>
+  );
+}
+
+export default App;
