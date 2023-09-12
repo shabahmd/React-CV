b19149c3507ea364a316f653341f3cea50727916
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-09-12 23:42:17 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 // src/components/About.js
 
 import React, { useState } from 'react';
+import './About.css'; // Import the CSS file
 
 function About() {
   const [showDetails, setShowDetails] = useState(false);
@@ -10,19 +11,17 @@ function About() {
   };
 
   return (
-    <section>
+    <section className="about"> {/* Apply the "about" class */}
       <h2>About Me</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
         cursus, lorem non aliquet interdum, urna elit blandit libero.
       </p>
-      {showDetails && (
-        <div>
-          <h3>Additional Details</h3>
-          <p>Additional information about yourself goes here.</p>
-        </div>
-      )}
-      <button onClick={toggleDetails}>
+      <div className={`details ${showDetails ? 'visible' : ''}`}> {/* Apply the "details" class conditionally */}
+        <h3>Additional Details</h3>
+        <p>Additional information about yourself goes here.</p>
+      </div>
+      <button className="show-details-button" onClick={toggleDetails}>
         {showDetails ? 'Hide Details' : 'Show Details'}
       </button>
     </section>
